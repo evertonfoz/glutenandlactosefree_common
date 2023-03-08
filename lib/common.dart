76f1810df0729e5aba2b9b0247d8b842e1c6d06f
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-03-08 07:29:11 -0300

    Update common.dart

--- lib/common.dart
+++ lib/common.dart
@@ -5,7 +5,9 @@ library common;
 
 export 'src/application/domain/profile_type.dart';
 export 'src/application/domain/application_consumer.dart';
+export 'src/application/domain/application_provider.dart';
 export 'src/application/data/profile_type.dart';
 export 'src/application/data/application_consumer.dart';
+export 'src/application/data/application_provider.dart';
 
 // TODO: Export any libraries intended for clients of this package.
