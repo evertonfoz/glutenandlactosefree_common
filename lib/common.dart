c6d14a69a5b17849887148aa34daf919576735bb
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-03-10 20:01:19 -0300

    Provider

--- lib/common.dart
+++ lib/common.dart
@@ -3,11 +3,12 @@
 /// More dartdocs go here.
 library common;
 
-export 'src/application/domain/application/profile_type.dart';
-export 'src/application/domain/application/consumer.dart';
-export 'src/application/domain/application/provider.dart';
 export 'src/application/data/profile_type.dart';
 export 'src/application/data/consumer.dart';
 export 'src/application/data/provider.dart';
+export 'src/application/domain/profile_type.dart';
+export 'src/application/domain/consumer.dart';
+export 'src/application/domain/provider.dart';
+export 'src/testimony/domain/testimony.dart';
 
 // TODO: Export any libraries intended for clients of this package.
