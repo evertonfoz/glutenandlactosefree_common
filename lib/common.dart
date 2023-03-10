f37d4df3feb0bff2286673a2ee89d33c768fe194
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-03-10 20:03:51 -0300

    Update common.dart

--- lib/common.dart
+++ lib/common.dart
@@ -9,6 +9,8 @@ export 'src/application/data/provider.dart';
 export 'src/application/domain/profile_type.dart';
 export 'src/application/domain/consumer.dart';
 export 'src/application/domain/provider.dart';
+export 'src/provider/data/provider.dart';
+export 'src/provider/domain/provider.dart';
 export 'src/testimony/domain/testimony.dart';
 export 'src/testimony/data/testimony.dart';
 
