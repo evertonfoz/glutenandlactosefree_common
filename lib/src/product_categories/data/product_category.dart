f0599765f23417c42d715f2858441d26f91e75a1
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-06-19 09:29:22 -0300

    Update product_category.dart

--- lib/src/product_categories/data/product_category.dart
+++ lib/src/product_categories/data/product_category.dart
@@ -22,8 +22,8 @@ class ProductCategoryModel extends ProductCategoryEntity {
 
   factory ProductCategoryModel.fromJson(Map<String, dynamic> json) {
     if (json['categories_providers_categories'] != null) {
-      json['productsModel'] = json['categories_providers_categories'];
-      json['productsModel'] = json['productsModel'];
+      json['productsModel'] = json['categories_providers_categories'][0]
+          ['provider_categories_products'];
     }
 
     return _$ProductCategoryModelFromJson(json);
