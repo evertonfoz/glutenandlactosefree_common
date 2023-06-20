4cd0fe49b9706afac9c5473fa464d447c704f1f0
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-06-20 16:25:06 -0300

    Update user_by_email.dart

--- lib/src/users/dtos/user_by_email.dart
+++ lib/src/users/dtos/user_by_email.dart
@@ -12,4 +12,24 @@ class UserByEmailDTO {
     required this.state,
     required this.token,
   });
+
+  factory UserByEmailDTO.fromJson(Map<String, dynamic> json) {
+    return UserByEmailDTO(
+      email: json['email'],
+      userProfileTypeID: json['userProfileTypeID'],
+      city: json['city'],
+      state: json['state'],
+      token: json['token'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'userProfileTypeID': userProfileTypeID,
+      'city': city,
+      'state': state,
+      'token': token,
+    };
+  }
 }
