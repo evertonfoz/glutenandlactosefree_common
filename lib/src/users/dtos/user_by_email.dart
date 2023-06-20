1df996326bc37e35811f062fcc362684172470dc
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-06-20 16:12:28 -0300

    Update user_by_email.dart

--- lib/src/users/dtos/user_by_email.dart
+++ lib/src/users/dtos/user_by_email.dart
@@ -3,11 +3,13 @@ class UserByEmailDTO {
   final int userProfileTypeID;
   final String city;
   final String state;
+  final String token;
 
   UserByEmailDTO({
     required this.email,
     required this.userProfileTypeID,
     required this.city,
     required this.state,
+    required this.token,
   });
 }
