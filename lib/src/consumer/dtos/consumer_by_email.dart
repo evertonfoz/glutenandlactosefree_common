73e7abb794873f816947ce485880d8502e73f247
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-06-21 18:41:42 -0300

    Update consumer_by_email.dart

--- lib/src/consumer/dtos/consumer_by_email.dart
+++ lib/src/consumer/dtos/consumer_by_email.dart
@@ -1,16 +1,20 @@
+import 'package:common/src/shared/data/address.dart';
+import 'package:common/src/shared/domain/address.dart';
+
 class ConsumerByEmailDTO {
   final int consumerID;
   final String? fullName;
   final String? aboutMe;
   final String? imageURL;
   final String? phoneNumber;
-  final String? street;
-  final String? number;
-  final String? complement;
-  final String? district;
-  final String? zipCode;
-  final String? city;
-  final String? state;
+  final AddressEntity? address;
+  // final String? street;
+  // final String? number;
+  // final String? complement;
+  // final String? district;
+  // final String? zipCode;
+  // final String? city;
+  // final String? state;
   final bool? glutenOrLactoseDontUseByChoice;
   final bool? glutenSensitive;
   final bool? lactoseIntolerance;
@@ -18,62 +22,73 @@ class ConsumerByEmailDTO {
 
   ConsumerByEmailDTO({
     this.aboutMe,
-    this.city,
-    this.complement,
+    this.address,
+    // this.city,
+    // this.complement,
     required this.consumerID,
     this.fullName,
     this.glutenOrLactoseDontUseByChoice,
     this.glutenSensitive,
     this.imageURL,
     this.lactoseIntolerance,
-    this.number,
+    // this.number,
     this.otherTypeOfSensitive,
     this.phoneNumber,
-    this.state,
-    this.street,
-    this.zipCode,
-    this.district,
+    // this.state,
+    // this.street,
+    // this.zipCode,
+    // this.district,
   });
 
   factory ConsumerByEmailDTO.fromJson(Map<String, dynamic> json) {
     return ConsumerByEmailDTO(
       aboutMe: json['aboutMe'],
-      city: json['city'],
-      complement: json['complement'],
+      address: AddressEntity(
+        street: json['street'],
+        number: json['number'],
+        complement: json['complement'],
+        district: json['district'],
+        zipCode: json['zipCode'],
+        city: json['city'],
+        state: json['state'],
+      ),
+      // city: json['city'],
+      // complement: json['complement'],
       consumerID: json['consumerID'],
       fullName: json['fullName'],
       glutenOrLactoseDontUseByChoice: json['glutenOrLactoseDontUseByChoice'],
       glutenSensitive: json['glutenSensitive'],
       imageURL: json['imageURL'],
       lactoseIntolerance: json['lactoseIntolerance'],
-      number: json['number'],
+      // number: json['number'],
       otherTypeOfSensitive: json['otherTypeOfSensitive'],
       phoneNumber: json['phoneNumber'],
-      state: json['state'],
-      street: json['street'],
-      zipCode: json['zipCode'],
-      district: json['district'],
+      // state: json['state'],
+      // street: json['street'],
+      // zipCode: json['zipCode'],
+      // district: json['district'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'aboutMe': aboutMe,
-      'city': city,
-      'complement': complement,
+      'address': (address as AddressModel).toJson(),
+      // 'city': city,
+      // 'complement': complement,
       'consumerID': consumerID,
       'fullName': fullName,
       'glutenOrLactoseDontUseByChoice': glutenOrLactoseDontUseByChoice,
       'glutenSensitive': glutenSensitive,
       'imageURL': imageURL,
       'lactoseIntolerance': lactoseIntolerance,
-      'number': number,
+      // 'number': number,
       'otherTypeOfSensitive': otherTypeOfSensitive,
       'phoneNumber': phoneNumber,
-      'state': state,
-      'street': street,
-      'zipCode': zipCode,
-      'district': district,
+      // 'state': state,
+      // 'street': street,
+      // 'zipCode': zipCode,
+      // 'district': district,
     };
   }
 }
