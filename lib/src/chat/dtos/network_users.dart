922989e81b511841edca78beb32dbc3c54d7efee
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-07-07 14:24:21 -0300

    Create network_users.dart

---
+++ lib/src/chat/dtos/network_users.dart
@@ -0,0 +1,29 @@
+import 'package:common/src/shared/domain/address.dart';
+
+class NetworkUserDTO {
+  final String? fullName;
+  final String? email;
+  final String? imageURL;
+
+  NetworkUserDTO({
+    this.fullName,
+    this.imageURL,
+    this.email,
+  });
+
+  factory NetworkUserDTO.fromJson(Map<String, dynamic> json) {
+    return NetworkUserDTO(
+      fullName: json['fullName'],
+      email: json['email'],
+      imageURL: json['imageURL'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'fullName': fullName,
+      'imageURL': imageURL,
+      'email': email,
+    };
+  }
+}
