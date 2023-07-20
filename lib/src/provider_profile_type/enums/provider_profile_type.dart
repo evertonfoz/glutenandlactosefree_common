d54163e36afa7741276a98663aa96c85ef997601
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-07-19 23:39:48 -0300

    Update provider_profile_type.dart

--- lib/src/provider_profile_type/enums/provider_profile_type.dart
+++ lib/src/provider_profile_type/enums/provider_profile_type.dart
@@ -10,3 +10,16 @@ enum ProviderProfileTypeEnum {
   kRestaurant,
   kOutro
 }
+
+final providerProfileTypeValues = {
+  ProviderProfileTypeEnum.kSnackBar: 1,
+  ProviderProfileTypeEnum.kDelivery: 2,
+  ProviderProfileTypeEnum.kRawMaterialSupplier: 3,
+  ProviderProfileTypeEnum.kHotelAndTheLike: 4,
+  ProviderProfileTypeEnum.kIndustryAndFoodProduction: 5,
+  ProviderProfileTypeEnum.kStore: 6,
+  ProviderProfileTypeEnum.kGastronomicProfessional: 7,
+  ProviderProfileTypeEnum.kRestaurant: 8,
+  ProviderProfileTypeEnum.kHealthcareProfessional: 9,
+  ProviderProfileTypeEnum.kOutro: 10,
+};
