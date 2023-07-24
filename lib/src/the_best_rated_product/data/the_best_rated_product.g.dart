6d54eca6c525bb631c7a7da513a98defba084b2f
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-07-24 09:34:41 -0300

    Update the_best_rated_product.g.dart

--- lib/src/the_best_rated_product/data/the_best_rated_product.g.dart
+++ lib/src/the_best_rated_product/data/the_best_rated_product.g.dart
@@ -8,8 +8,22 @@ part of 'the_best_rated_product.dart';
 
 TheBestRatedProductModel _$TheBestRatedProductModelFromJson(
         Map<String, dynamic> json) =>
-    TheBestRatedProductModel();
+    TheBestRatedProductModel(
+      productID: json['productID'] as int?,
+      imageURL: json['imageURL'] as String?,
+      blurToImage: json['blurToImage'] as String?,
+      dominantColor: json['dominantColor'] as String?,
+      foreGroundColor: json['foreGroundColor'] as String?,
+      rattings: (json['rattings'] as num?)?.toDouble(),
+    );
 
 Map<String, dynamic> _$TheBestRatedProductModelToJson(
         TheBestRatedProductModel instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'productID': instance.productID,
+      'imageURL': instance.imageURL,
+      'blurToImage': instance.blurToImage,
+      'dominantColor': instance.dominantColor,
+      'foreGroundColor': instance.foreGroundColor,
+      'rattings': instance.rattings,
+    };
