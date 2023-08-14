ab4212ceabcc7753e5af9107bdba63566becfdc8
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-08-14 16:27:59 -0300

    Update recipe.dart

--- lib/src/recipes/data/recipe.dart
+++ lib/src/recipes/data/recipe.dart
@@ -33,28 +33,6 @@ class RecipeModel extends RecipeEntity {
     json['imagesModel'] = json['recipes_recipe_images'];
     json['providerModel'] = json['user_provider']['user_provider'];
     json['consumerModel'] = json['user_consumer']['user_consumer'];
-    if (json['consumerModel'] != null) {
-      json['consumerModel']['address'] = AddressEntity(
-        street: json['recipes_user']['user_consumer']['street'],
-        number: json['recipes_user']['user_consumer']['number'],
-        complement: json['recipes_user']['user_consumer']['complement'],
-        district: json['recipes_user']['user_consumer']['district'],
-        zipCode: json['recipes_user']['user_consumer']['zipCode'],
-        city: json['eventsrecipes_user_user']['user_consumer']['city'],
-        state: json['recipes_user']['user_consumer']['state'],
-      ).toJson();
-    }
-    if (json['providerModel'] != null) {
-      json['providerModel']['address'] = AddressEntity(
-        street: json['recipes_user']['user_provider']['street'],
-        number: json['recipes_user']['user_provider']['number'],
-        complement: json['recipes_user']['user_provider']['complement'],
-        district: json['recipes_user']['user_provider']['district'],
-        zipCode: json['recipes_user']['user_provider']['zipCode'],
-        city: json['recipes_user']['user_provider']['city'],
-        state: json['recipes_user']['user_provider']['state'],
-      ).toJson();
-    }
 
     return _$RecipeModelFromJson(json);
   }
