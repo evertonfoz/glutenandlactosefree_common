5f8974afeca51a37db1e316100c64f7ab34a12a9
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-09-26 08:56:47 -0300

    Update provider.dart

--- lib/src/provider/data/provider.dart
+++ lib/src/provider/data/provider.dart
@@ -105,7 +105,7 @@ class ProviderModel extends ProviderEntity {
     ratingSum = providerModel.ratingSum;
     ratingCount = providerModel.ratingCount;
     isFavorited = providerModel.isFavorited;
-    providerProfileTypes = providerModel.providerProfileTypes??[];
+    providerProfileTypesModel = providerModel.providerProfileTypes??[];
   }
 
   factory ProviderModel.fromJson(Map<String, dynamic> json) {
