d3e4a194db9f473724a1070a5e024070404a48af
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2023-10-17 18:14:14 -0300

    Update provider.dart

--- lib/src/provider/data/provider.dart
+++ lib/src/provider/data/provider.dart
@@ -1,4 +1,6 @@
 // flutter packages pub run build_runner build --delete-conflicting-outputs
+// ignore_for_file: must_be_immutable
+
 import 'package:common/common.dart';
 import 'package:json_annotation/json_annotation.dart';
 
@@ -106,6 +108,87 @@ class ProviderModel extends ProviderEntity {
     ratingCount = providerModel.ratingCount;
     isFavorited = providerModel.isFavorited;
     registerProviderProfileTypes(providerModel.providerProfileTypes ?? []);
+  }
+
+  ProviderModel copyWith({
+    int? providerPlanID,
+    int? providerID,
+    String? email,
+    String? brandName,
+    String? responsibleName,
+    String? brandImageURL,
+    String? howKnowsAboutUs,
+    String? aboutServices,
+    String? responsibleImageURL,
+    String? advertiseImageURL,
+    double? ratting,
+    String? balloonText,
+    String? advertiseText,
+    String? phoneNumber,
+    String? observations,
+    bool? safeFood,
+    bool? animalMilkFree,
+    bool? glutenFree,
+    bool? lactoseFree,
+    bool? vegan,
+    bool? vegetarian,
+    List<ProviderServiceDayModel>? providerServiceDays,
+    String? blurToBrandImage,
+    bool? noPreservatives,
+    bool? frozen,
+    bool? delivery,
+    bool? acceptOrders,
+    bool? haveResellers,
+    AddressEntity? address,
+    bool? resellersInListView,
+    List<ProductModel>? products,
+    String? brandDominantColor,
+    String? foreGroundColor,
+    double? ratingSum,
+    int? ratingCount,
+    bool? isFavorited,
+    List<ProviderProfileTypeModel>? providerProfileTypes,
+  }) {
+    return ProviderModel(
+      providerPlanID: providerPlanID ?? this.providerPlanID,
+      providerID: providerID ?? this.providerID,
+      email: email ?? this.email,
+      brandName: brandName ?? this.brandName,
+      responsibleName: responsibleName ?? this.responsibleName,
+      brandImageURL: brandImageURL ?? this.brandImageURL,
+      howKnowsAboutUs: howKnowsAboutUs ?? this.howKnowsAboutUs,
+      aboutServices: aboutServices ?? this.aboutServices,
+      responsibleImageURL: responsibleImageURL ?? this.responsibleImageURL,
+      advertiseImageURL: advertiseImageURL ?? this.advertiseImageURL,
+      ratting: ratting ?? this.ratting,
+      balloonText: balloonText ?? this.balloonText,
+      advertiseText: advertiseText ?? this.advertiseText,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      observations: observations ?? this.observations,
+      safeFood: safeFood ?? this.safeFood,
+      animalMilkFree: animalMilkFree ?? this.animalMilkFree,
+      glutenFree: glutenFree ?? this.glutenFree,
+      lactoseFree: lactoseFree ?? this.lactoseFree,
+      vegan: vegan ?? this.vegan,
+      vegetarian: vegetarian ?? this.vegetarian,
+      providerServiceDayModel: providerServiceDays ?? providerServiceDayModel,
+      blurToBrandImage: blurToBrandImage ?? this.blurToBrandImage,
+      noPreservatives: noPreservatives ?? this.noPreservatives,
+      frozen: frozen ?? this.frozen,
+      delivery: delivery ?? this.delivery,
+      acceptOrders: acceptOrders ?? this.acceptOrders,
+      haveResellers: haveResellers ?? this.haveResellers,
+      address: address ?? this.address,
+      resellersInListView: resellersInListView ?? this.resellersInListView,
+      productsModel: products ?? productsModel,
+      brandDominantColor: brandDominantColor ?? this.brandDominantColor,
+      foreGroundColor: foreGroundColor ?? this.foreGroundColor,
+      ratingSum: ratingSum ?? this.ratingSum,
+      ratingCount: ratingCount ?? this.ratingCount,
+      isFavorited: isFavorited ?? this.isFavorited,
+      providerProfileTypesModel:
+          providerProfileTypes ?? providerProfileTypesModel,
+    );
   }
 
   factory ProviderModel.fromJson(Map<String, dynamic> json) {
